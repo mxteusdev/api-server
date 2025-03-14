9365cccf83900160dabfd39b0118b3e244795a9e
mateus.rodrigues <mateus.rodrigues@mkcranes.com.br>
2025-03-14 16:25:01 +0000

    base structure for Models

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,3 +1,34 @@
 import { DataTypes, Model } from "sequelize";
 import sequelize from "../config/database";
 
+class User extends Model {
+    public id!: number;
+    public name!: string;
+    public email!: string;
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'User',
+        tableName: 'users',
+    }
+);
+
+export default User;
